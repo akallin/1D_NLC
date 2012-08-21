2f1e6764af1742e54f04f966b752d7102ec7d6ca
akallin <ann.kallin@gmail.com>
2012-08-21 15:05:09 -0400

    just got order18results.  Took a long time to run on my laptop so I saved the results and added them to the repo

--- 2D_EE/NLC_2D_EE.cpp
+++ 2D_EE/NLC_2D_EE.cpp
@@ -50,7 +50,7 @@ int main(){
     double RunningSumEntropy(0), RunningSumMagnetization;
     double mag;
 
-    ReadGraphsFromFile(fileGraphs, "order16rectangles.dat");
+    ReadGraphsFromFile(fileGraphs, "order18rectangles.dat");
     //ReadGraphsFromFile(fileGraphs,"L16pbc.dat");
 
     ofstream fout("output_1D.dat");
